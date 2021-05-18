666ba9f6b1c178e492deab03ff0cd6e2b6b41a68
Arthur Cardoso de Jesus <arthurcardosoj@gmail.com>
2021-05-18 18:28:08 -0300

    Uma atividade feita no dia 13/05/2021 Loop simples que mostra numeros imparesn tre 100 e 200

---
+++ src/desafio9exer4/Desafio9Exer4.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio9exer4;
+
+/**
+ *
+ * @author baung
+ */
+public class Desafio9Exer4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int numero;
+        numero = 101;
+        do{
+            System.out.println(numero);
+            numero += 2;
+            
+        }while(200>numero);
+
+    }
+
+}
